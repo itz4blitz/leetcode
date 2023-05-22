2a00247ab9adeb63ecd6e935adbb7ff35ac42277
Justin Scroggins <justinscroggins@outlook.com>
2023-05-22 16:32:12 -0400

    Solved challenge remove element

---
+++ .vscode/27.remove-element.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=27 lang=typescript
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+function removeElement(nums: number[], val: number): number {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+};
+// @lc code=end
+
